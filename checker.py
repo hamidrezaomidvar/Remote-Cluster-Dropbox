f680ed36b65fe90ad4ac5b5b2dbe05889d0dae16
Hamidreza Omidvar <omidvar.hamidreza@gmail.com>
2019-05-12 13:20:45 +0100

    adding the modifying capability

--- checker.py
+++ checker.py
@@ -1,15 +1,35 @@
-import os, time
+import os
+import time
 from subprocess import call
-path_to_watch = "."
-before = dict ([(f, None) for f in os.listdir (path_to_watch)])
-dropbx='/home/users/homidvar/Dropbox-Uploader/dropbox_uploader.sh'
+
+path_to_watch = "./test_folder/"
+
+before = dict([(f, os.stat(path_to_watch+f).st_mtime)
+              for f in os.listdir(path_to_watch)])
+#dropbx = '/Users/hamidrezaomidvar/Desktop/Dropbox-Uploader/dropbox_uploader.sh'
+dropbx = '/Users/hamidrezaomidvar/Desktop/Dropbox-Uploader/dropbox_uploader.sh'
+
 while 1:
-  time.sleep (2)
-  after = dict ([(f, None) for f in os.listdir (path_to_watch)])
-  added = [f for f in after if not f in before]
-  removed = [f for f in before if not f in after]
-  if added:
-      for f in added:
-          call([dropbx,'upload',f,'.'])	
-  #if removed: print "Removed: ", ", ".join (removed)
-  before = after
+
+    time.sleep(2)
+
+    after = dict([(f, os.stat(path_to_watch+f).st_mtime)
+                  for f in os.listdir(path_to_watch)])
+
+    added = [f for f in after if not f in before]
+
+    modified = [f for f in after if f in before
+                and after[f] != before[f]]
+
+    if added:
+        for f in added:
+            bash_cmd = [dropbx, 'upload', path_to_watch+f, '/']
+            call(bash_cmd)
+
+    if modified:
+        for f in modified:
+            print(os.stat(path_to_watch+f).st_mtime)
+            bash_cmd = [dropbx, 'upload', path_to_watch+f, '/']
+            call(bash_cmd)
+
+    before = after
